26088d2e59bff5652769068d9bf778f87266ff8a
paul <paul.wieland@catena.software>
2023-03-15 10:27:27 +0100

    Add OpenFaaS health checks for python template

--- xfaas-function-python3.9/xfaas_function.py
+++ xfaas-function-python3.9/xfaas_function.py
@@ -4,6 +4,23 @@ from function import handler
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def function_entry():
     return handler.function(request)
+
+
+# Required for OpenFaaS health check
+@app.route("/_/health")
+def health():
+    return app.make_response(("Ok", 200))
+
+
+# Required for OpenFaaS readiness check
+@app.route("/_/ready")
+def ready():
+    return app.make_response(("Ok", 200))
+
+
+# Local testing
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)

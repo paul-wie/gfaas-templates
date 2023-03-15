00c5c2f19652156b39cdff25de31832167659773
paul <paul.wieland@catena.software>
2023-03-15 10:52:19 +0100

    Add health and readiness endpoints for OpenFaaS

--- xfaas-function-go1.19/main.go
+++ xfaas-function-go1.19/main.go
@@ -9,6 +9,8 @@ import (
 
 func main() {
 	fmt.Printf("XFaaS Function is listening on port 8080\n")
+	http.HandleFunc("/_/health", Health)
+	http.HandleFunc("/_/ready", Ready)
 	http.HandleFunc("/", Function)
 	err := http.ListenAndServe(":8080", nil)
 
@@ -19,4 +21,21 @@ func main() {
 		os.Exit(1)
 	}
 
+}
+
+/*
+Health and Ready endpoint for OpenFaaS
+*/
+func Health(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Ok")
+	if err != nil {
+		return
+	}
+}
+
+func Ready(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Ok")
+	if err != nil {
+		return
+	}
 }

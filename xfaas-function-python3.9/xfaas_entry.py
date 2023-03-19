1864a321934e9fc310df30307789fddc0cf51f61
paul <paul.wieland@catena.software>
2023-03-19 19:15:37 +0100

    Add new core library version

--- xfaas-function-python3.9/xfaas_entry.py
+++ xfaas-function-python3.9/xfaas_entry.py
@@ -1,6 +1,6 @@
 from flask import make_response
 from xfaas_core_python3.xfunction import XFunction
-from xfaas_core_python3.xrunner import get_server, run_app
+from xfaas_core_python3.xrunner import get_app, run_app
 
 
 # MyFunctionEntry is used as entry point by the xfaas_core_python3 package as it implements XFunction
@@ -12,7 +12,7 @@ class MyFunctionEntry(XFunction):
 
 
 # Required for gunicorn, Set MyFunctionEntry as target_function
-app = get_server(MyFunctionEntry())
+app = get_app(MyFunctionEntry())
 
 # Run the server locally
 if __name__ == '__main__':
